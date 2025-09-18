bcb32d1654dadf5218a6ce44d6e94cd0524788a7
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-09-18 17:27:16 +0300

    Added multibyte count and going to refactor the entire code struct

--- word-count/ccwc.c
+++ word-count/ccwc.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[]) {
     char *command = argv[1];
     char *filename = argv[2];
 
+    if ((sizeof(argv)/sizeof(argv[0])) == 1) {
+        filename = argv[1]
+    }
+
     FILE *fptr = fopen(filename, "r");
 
     if (fptr == NULL) {
@@ -38,6 +42,16 @@ int main(int argc, char *argv[]) {
                         count++;
                     }
                 }
+            }
+            
+            if (strcmp(command, "-m") == 0) {
+                if ((ch & 0xC0) != 0x80) {
+                    count++;
+                }
+            } 
+
+            if (command == NULL) {
+
             }
         }
         printf("%d%s%s\n", count, " ", filename);
